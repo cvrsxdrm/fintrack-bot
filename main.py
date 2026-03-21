bd1593234054ebbbf6a54cb8c76d1a341c9c0c81
cvrsxdrm <chillbabyfort@gmail.com>
2026-03-21 23:56:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import requests
 import os
-import sqlite3
 import telebot
 from telebot import types
+from config import EXCHANGE_KEY 
 from config import TOKEN
 from datetime import datetime
 
 bot = telebot.TeleBot(TOKEN)
-
-CURRENCIES = {"🪙 BTC": "bitcoin", "🔹 ETH": "ethereum"}
-MONEY = {"USD": "tether", "EUR": "eur", "RUB": "rub", "JPY": "jpy"}
-
-CATEGORIES = ["Еда", "Транспорт", "Развлечения"]
+CURRENCIES = {"🪙 BTC": "bitcoin", "🔹 ETH": "ethereum", "USDT": "tether", "SOL": "solana", "BNB": "binancecoin", "XRP": "ripple", "USDC": "usd-coin", "ADA": "cardano", "DOGE": "dogecoin", "TRX": "tron"}
+CATEGORIES = ["🍕 Еда", "🚌 Транспорт", "🎮 Развлечения"]
 
 def main_menu_markup():
 
@@ -25,6 +22,18 @@ def main_menu_markup():
         types.InlineKeyboardButton("💳 Валюты", callback_data="check_money"))
     
     return markup
+
+def get_fiat_rate(base="USD", target="RUB"):
+    url = f"https://v6.exchangerate-api.com/v6/{EXCHANGE_KEY}/pair/{base}/{target}"
+
+    try:
+        response = requests.get(url, timeout=5)
+        data = response.json()
+        if data.get('result') == 'success':
+            return data['conversion_rate']
+        return None
+    except:
+        return None
 
 def get_price(coin_id, vs_currency="usd"):
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={vs_currency}"
@@ -54,16 +63,12 @@ def choose_currency(call):
     markup = types.InlineKeyboardMarkup(row_width=2)
 
     for coin_name in CURRENCIES.keys():
-
         item = types.InlineKeyboardButton(text=coin_name, callback_data=f"crypto_{coin_name}")
+        markup.add(item) 
+    
+    markup.add(types.InlineKeyboardButton("⬅️ Назад", callback_data="back_to_main"))
 
-        markup.add(item)
-
-    button_back = types.InlineKeyboardButton("⬅️ Назад", callback_data="back_to_main")
-
-    markup.add(button_back)
-
-    bot.edit_message_text("📈 Выбери монету или вернись назад:", message_id=call.message.message_id, chat_id=call.message.chat.id, reply_markup=markup)
+    bot.edit_message_text("📈 Выбери монету:", chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("crypto_"))
 def ask_fiat_currency(call):
@@ -107,17 +112,18 @@ def callback_price(call):
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("calc_"))
 def callback_calc(call):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+
+    button_back = types.InlineKeyboardButton("⬅️ Назад", callback_data="back_to_main")
+
+    markup.add(button_back)
+
     bot.answer_callback_query(call.id)
     _, price, sym = call.data.split("_")
     msg = bot.send_message(call.message.chat.id, f"🪙 Сколько монет у тебя есть? (Результат будет в {sym})\n\nВведи число:")
     bot.register_next_step_handler(msg, calculate, float(price), sym)
 
 def calculate(message, price, sym):
-    if message.text == "❌ Отмена":
-        bot.send_message(message.chat.id, "🏠 Возвращаемся в главное меню.", reply_markup=None)
-        start_handler(message)
-        return
-
     try:
         amount = float(message.text.replace(',', '.'))
         result = round(price * amount, 2)
@@ -125,12 +131,8 @@ def calculate(message, price, sym):
         start_handler(message)
 
     except ValueError:
-
-        markup = types.InlineKeyboardMarkup(row_width=2)
-
-        markup.add(types.InlineKeyboardButton("❌ Отмена"))
-
-        bot.send_message(message.chat.id, "❌ Введи числовое значение!", reply_markup=markup)
+        bot.send_message(message.chat.id, "❌ Введи числовое значение!")
+        start_handler(message)
 
 # блок расходы
 @bot.callback_query_handler(func=lambda call: call.data == "add_spendings")
@@ -152,6 +154,8 @@ def choose_category(call):
 @bot.callback_query_handler(func=lambda call: call.data == "back_to_main")
 def back_to_main(call):
     bot.answer_callback_query(call.id)
+    # Сбрасываем ожидание ввода, если оно было активно
+    bot.clear_step_handler_by_chat_id(chat_id=call.message.chat.id)
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="🌟 Привет!\n\n💸 Я бот финансист...", reply_markup=main_menu_markup())
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("cat_"))
@@ -163,13 +167,20 @@ def ask_spending_currency(call):
     btn_eur = types.InlineKeyboardButton("💶 EUR", callback_data=f"spend_{category}_eur")
     btn_rub = types.InlineKeyboardButton("💳 RUB", callback_data=f"spend_{category}_rub")
     btn_jpy = types.InlineKeyboardButton("💴 JPY", callback_data=f"spend_{category}_jpy")
+    button_back = types.InlineKeyboardButton("⬅️ Назад", callback_data="back_to_main")
 
-    markup.add(btn_usd, btn_eur, btn_rub, btn_jpy)
+    markup.add(btn_usd, btn_eur, btn_rub, btn_jpy, button_back)
 
     bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text=f"В какой валюте была трата на категорию {category}?", reply_markup=markup,)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("spend_"))
 def callback_spending_currency(call):
+    markup = types.InlineKeyboardMarkup()
+
+    button_back = types.InlineKeyboardButton("⬅️ Назад", callback_data="back_to_main")
+
+    markup.add(button_back)
+
     bot.answer_callback_query(call.id)
     _, category, fiat = call.data.split("_")
     msg = bot.send_message(call.message.chat.id, f"Сколько ты потратил в {fiat.upper()}?")
@@ -177,11 +188,6 @@ def callback_spending_currency(call):
     bot.register_next_step_handler(msg, how_much_spend, category, fiat)
 
 def how_much_spend(message, category, fiat):
-    if message.text == "❌ Отмена":
-        bot.send_message(message.chat.id, "🏠 Возвращаемся в главное меню.")
-        start_handler(message)
-        return
-
     try:
         amount = float(message.text.replace(',', '.'))
         filename = f"spendings_{message.chat.id}.txt"
@@ -194,26 +200,32 @@ def how_much_spend(message, category, fiat):
 
     except ValueError:
         bot.send_message(message.chat.id, "❌ Введи числовое значение!")
-        bot.register_next_step_handler(message, how_much_spend, category, fiat)
+        start_handler(message)
 
 @bot.callback_query_handler(func=lambda call: call.data == "read_spendings")
 def send_spendings(call):
+    markup = types.InlineKeyboardMarkup()
+
+    button_back = types.InlineKeyboardButton("⬅️ Назад", callback_data="back_to_main")
+
+    markup.add(button_back)
+
     bot.answer_callback_query(call.id)
     filename = f"spendings_{call.message.chat.id}.txt"
 
     if not os.path.exists(filename):
         bot.send_message(call.message.chat.id, "❌ Список пока пуст!")
-        return
+        start_handler(call.message)
 
     with open(filename, "r", encoding="utf-8") as f:
         spendings = f.readlines()
 
     if not spendings:
         bot.send_message(call.message.chat.id, "❌ Список пока пуст!")
-        return
+        start_handler(call.message)
 
     text_report = "📋 Твои траты:\n\n" + "".join(spendings)
-    bot.send_message(call.message.chat.id, text_report)
+    bot.send_message(call.message.chat.id, text_report, reply_markup=markup)
 
 # блок для удаления файла
 @bot.callback_query_handler(func=lambda call: call.data == "clear_history")
@@ -224,7 +236,6 @@ def confirm_clear(call):
     btn_yes = types.InlineKeyboardButton("✅ Да, очистить", callback_data="clear_yes")
     btn_no = types.InlineKeyboardButton("❌ Нет, оставить", callback_data="clear_no")
     markup.add(btn_yes, btn_no)
-
 
     bot.send_message(call.message.chat.id, "❗ Ты уверен, что хочешь удалить все записи?", reply_markup=markup)
 
@@ -238,48 +249,55 @@ def callback_clear(call):
         if os.path.exists(filename):
             os.remove(filename)
             bot.edit_message_text("✅ История успешно удалена!", call.message.chat.id, call.message.message_id)
+            start_handler(call.message)
 
         else:
             bot.edit_message_text("❌ Файл и так пуст.", call.message.chat.id, call.message.message_id)
+            start_handler(call.message)
 
     else:
         bot.edit_message_text("🏠 Удаление отменено.", call.message.chat.id, call.message.message_id)
+        start_handler(call.message)
 
-# 1. Выбор валюты
+# 1. Выбор базовой валюты
 @bot.callback_query_handler(func=lambda call: call.data == "check_money")
-def choose_money(call):
+def exchange_menu(call):
     bot.answer_callback_query(call.id)
     markup = types.InlineKeyboardMarkup(row_width=2)
-    for name, code in MONEY.items():
-        btn = types.InlineKeyboardButton(name, callback_data=f"fiatconv_{code}")
-        markup.add(btn)
+    # Список валют для обмена
+    fiats = {"USD ➡️ RUB": "USD_RUB", "EUR ➡️ RUB": "EUR_RUB", "USD ➡️ EUR": "USD_EUR"}
+    
+    for name, res in fiats.items():
+        markup.add(types.InlineKeyboardButton(name, callback_data=f"exc_{res}"))
+    
+    markup.add(types.InlineKeyboardButton("⬅️ Назад", callback_data="back_to_main"))
+    bot.edit_message_text("💱 Что во что переводим?", call.message.chat.id, call.message.message_id, reply_markup=markup)
 
-    bot.edit_message_text("📈 Выбери валюту, которую хочешь купить за USD:", call.message.chat.id, call.message.message_id, reply_markup=markup)
-
-# 2. Ловим выбор и спрашиваем сумму
-@bot.callback_query_handler(func=lambda call: call.data.startswith("fiatconv_"))
-def ask_amount_fiat(call):
+# 2. Ловим выбор и запрашиваем сумму
+@bot.callback_query_handler(func=lambda call: call.data.startswith("exc_"))
+def ask_exc_amount(call):
     bot.answer_callback_query(call.id)
-    target_fiat = call.data.split("_")[1]
-    msg = bot.send_message(call.message.chat.id, f"Введите сумму в USD для перевода в {target_fiat.upper()}:")
-    # Передаем выбранную валюту дальше в функцию расчета
-    bot.register_next_step_handler(msg, calculate_fiat, target_fiat)
+    _, pair = call.data.split("_", 1) # Получим например "USD_RUB"
+    base, target = pair.split("_")
+    
+    msg = bot.send_message(call.message.chat.id, f"Введите сумму в {base} для перевода в {target}:")
+    bot.register_next_step_handler(msg, perform_exchange, base, target)
 
-# 3. Сама функция расчета
-def calculate_fiat(message, target_fiat):
+# 3. Финальный расчет
+def perform_exchange(message, base, target):
     try:
-        usd_amount = float(message.text.replace(',', '.'))
-        # Используем твою функцию get_price.
-        # Т.к. CoinGecko — крипто-сервис, берем tether как эквивалент USD
-        rate = get_price("tether", target_fiat)
-
+        amount = float(message.text.replace(',', '.'))
+        rate = get_fiat_rate(base, target)
+        
         if rate:
-            result = round(usd_amount * rate, 2)
-            bot.send_message(message.chat.id, f"✅ {usd_amount} USD = {result} {target_fiat.upper()}\n(Курс: {rate})")
+            res = round(amount * rate, 2)
+            bot.send_message(message.chat.id, f"✅ {amount} {base} = {res} {target}\nКурс: {rate}")
         else:
-            bot.send_message(message.chat.id, "⚠️ Ошибка API. Попробуй позже.")
+            bot.send_message(message.chat.id, "⚠️ Ошибка API.")
+        start_handler(message)
     except ValueError:
-        bot.send_message(message.chat.id, "❌ Введи числовое значение!")
+        bot.send_message(message.chat.id, "❌ Нужно число!")
+        start_handler(message)
 
 print("Bot is running...")
 bot.infinity_polling(skip_pending=True)
